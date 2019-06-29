dab8e1403bf350a683379d8b88f1a33a11cdc13d
Alexander Bercow <alexanderbercow@AlexandMBPBlack.home>
2019-06-29 11:30:45 -0400

    added styling, state, error message

--- src/App.js
+++ src/App.js
@@ -7,25 +7,82 @@ require('dotenv').config()
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 class App extends React.Component {
-
-  getWeather = async (e) => {
-    e.preventDefault();
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
-    const data = await api_call.json();
-    console.log(data);
+  //Defines State
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
   }
 
+  
+  getWeather = async (e) => {
+    e.preventDefault();
+    //Creates the inputs
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    //API call + convert to JSON
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+    const data = await api_call.json();
+
+    //When city/country entered, get data from JSON
+    if (city && country) {
+      console.log(data);
+      this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      error: "",
+    });
+    //When no city/country, return error
+    } else {
+      this.setState({
+      temperature: undefined,
+      city: undefined,
+      country: undefined,
+      humidity: undefined,
+      description: undefined,
+      error: "Please enter City & Country",
+      });
+    }
+  }
+
+  //Render to browser
   render() {
     return(
       <div>
-        <Titles />
-        <Form getWeather={this.getWeather}/>
-        <Weather />
+      <div className="wrapper">
+        <div className="main">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-5 title-container">
+                <Titles />
+              </div>
+              <div className="col-xs-7 form-container">
+                <Form getWeather={this.getWeather}/>
+                <Weather temperature={this.state.temperature}
+                  city={this.state.city}
+                  country={this.state.country}
+                  humidity={this.state.humidity}
+                  description={this.state.description}
+                  error={this.state.error}
+                  />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       </div>
     );
   };
 }
+
+
+
+
 
 export default App;
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,13 +1,28 @@
 import React from "react";
 
-class Weather extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>Weather Comp</h1>
-            </div>
-        );
-    }
+const Weather = (props) => (
+    <div className="weather__info">
+        {props.city && props.country && <p className="weather__key">Location:
+            <span className="weather__value">
+                {props.city}, {props.country}</span>
+        </p>
 }
-
+        {props.temperature && <p className="weather__key">tempurature:
+            <span className="weather__value">{props.temperature}</span>
+        </p>
+}
+        {props.humidity && <p className="weather__key">Humidity:
+            <span className="weather__value">{props.humidity}</span>
+        </p>
+}
+        {props.description && <p className="weather__key">Conditions:
+            <span className="weather__value">{props.description}</span>
+        </p>
+}
+        {props.error && <p>
+            <span className="weather__error">{props.error}</span>
+        </p>
+}
+    </div>
+);
 export default Weather;
